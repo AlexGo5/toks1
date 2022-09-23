75c27a939526c0f8afd4dedd2c04a19f9e1b02a9
AlexGo5 <sasha.gorgun@gmail.com>
2022-09-23 14:33:25 +0300

    all

--- main.go
+++ main.go
@@ -1,24 +1,43 @@
 package main
 
 import (
-	"fyne.io/fyne/v2"
-	"fyne.io/fyne/v2/app"
-	"fyne.io/fyne/v2/container"
-	"fyne.io/fyne/v2/widget"
+	"log"
+
+	"github.com/tarm/serial"
+	// "fyne.io/fyne/v2"
+	// "fyne.io/fyne/v2/app"
+	// "fyne.io/fyne/v2/container"
+	// "fyne.io/fyne/v2/widget"
 )
 
 func main() {
-	a := app.New()
-	w := a.NewWindow("Serial-port")
-	w.Resize(fyne.NewSize(400, 320))
-	entry := widget.NewEntry()
-	answer := widget.NewLabel("A:")
-	descr := widget.NewLabel("B:")
-	contain := container.NewHSplit(entry, answer)
-	w.SetContent(container.NewHSplit(
-		contain,
-		descr,
-	))
-	//}
-	w.ShowAndRun()
+	c := &serial.Config{Name: "COM1", Baud: 115200}
+	s, err := serial.OpenPort(c)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	n, err := s.Write([]byte("test"))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	buf := make([]byte, 128)
+	n, err = s.Read(buf)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Print("%q", buf[:n])
+	// a := app.New()
+	// w := a.NewWindow("Serial-port")
+	// w.Resize(fyne.NewSize(400, 320))
+	// entry := widget.NewEntry()
+	// answer := widget.NewLabel("A:")
+	// descr := widget.NewLabel("B:")
+	// contain := container.NewHSplit(entry, answer)
+	// w.SetContent(container.NewHSplit(
+	// 	contain,
+	// 	descr,
+	// ))
+	// w.ShowAndRun()
 }
