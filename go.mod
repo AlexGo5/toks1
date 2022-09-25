745fe2153ec2ebc0b5bd8fec58428ae689b53399
AlexGo5 <sasha.gorgun@gmail.com>
2022-09-26 01:43:22 +0300

    FINISH!!!

--- go.mod
+++ go.mod
@@ -2,10 +2,14 @@ module toks1
 
 go 1.18
 
-require fyne.io/fyne/v2 v2.2.3
+require (
+	fyne.io/fyne/v2 v2.2.3
+	go.bug.st/serial v1.4.0
+)
 
 require (
 	fyne.io/systray v1.10.1-0.20220621085403-9a2652634e93 // indirect
+	github.com/creack/goselect v0.1.2 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/fredbi/uri v0.0.0-20181227131451-3dcfdacbaaf3 // indirect
 	github.com/fsnotify/fsnotify v1.5.4 // indirect
--- main.go
+++ main.go
@@ -3,23 +3,88 @@ package main
 import (
 
 	//"github.com/tarm/serial"
+
+	"strings"
+	"time"
+
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/widget"
+	"go.bug.st/serial"
 )
 
+var descr *widget.Label
+
+func catchErr() {
+	str := "ERROR [Connection is not established]\n\nInput any key to exit"
+	descr.SetText(str)
+
+}
+
+func workCOM1(port serial.Port, mode *serial.Mode, str string) {
+	_, err := port.Write([]byte(str))
+	if err != nil {
+		catchErr()
+	}
+}
+
+func workCOM2(port serial.Port, mode *serial.Mode) string {
+
+	buff := make([]byte, 100)
+	n, err := port.Read(buff)
+	if err != nil {
+		catchErr()
+	}
+	return string(buff[:n])
+}
+
 func main() {
+	//Open the first serial port detected at 9600bps N81
 	a := app.New()
 	w := a.NewWindow("Serial-port")
-	w.Resize(fyne.NewSize(400, 320))
-	entry := widget.NewEntry()
-	answer := widget.NewLabel("A:")
-	descr := widget.NewLabel("B:")
-	contain := container.NewHSplit(answer, descr)
-	w.SetContent(container.NewHSplit(
-		entry,
-		contain,
-	))
+	w.Resize(fyne.NewSize(600, 600))
+	entry := widget.NewMultiLineEntry()
+	entry.Wrapping = fyne.TextWrapBreak
+	answer := widget.NewLabel("")
+	answer.Wrapping = fyne.TextWrapBreak
+	descr = widget.NewLabel("The connection is established")
+	contain := container.NewGridWithColumns(3, entry, answer, descr)
+	w.SetContent(contain)
+
+	mode := &serial.Mode{
+		BaudRate: 9600,
+		Parity:   serial.NoParity,
+		DataBits: 8,
+		StopBits: serial.OneStopBit,
+	}
+	port1, err := serial.Open("COM1", mode)
+	if err != nil {
+		catchErr()
+	}
+	port2, err := serial.Open("COM2", mode)
+	if err != nil {
+		catchErr()
+	}
+
+	go func() {
+		var strMain string
+		for range time.Tick(time.Second / 8) {
+			str := entry.Text
+			if len(str) == len(strMain) {
+				continue
+			}
+			if !strings.HasPrefix(str, strMain) {
+				entry.SetText(strMain)
+			} else {
+				_, chars, _ := strings.Cut(str, strMain)
+				workCOM1(port1, mode, chars)
+				newChars := workCOM2(port2, mode)
+				//fmt.Println(newChars, "!!!")
+				strMain += newChars
+				answer.SetText(strMain)
+			}
+		}
+	}()
 	w.ShowAndRun()
 }
